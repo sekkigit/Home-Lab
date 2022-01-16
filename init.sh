eb0d83e9cf41729cb619260e293b012bf9a3df2c
sekkigit <sekiuredjaji@gmail.com>
2022-01-16 22:52:07 +0100

    1

--- init.sh
+++ init.sh
@@ -170,7 +170,8 @@ banner2 "P L E X  S E T U P"
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/$PLEXserver.list
 
-apt update && echo y | apt install plexmediaserver -y
+apt update
+echo y | apt install plexmediaserver -y
 
 service plexmediaserver start
 cat <<EOF > /etc/ufw/applications.d/plexmediaserver
