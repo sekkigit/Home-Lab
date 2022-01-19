97a953f0000a69fbd62bd3929b784b75183627ed
sekkigit <sekiuredjaji@gmail.com>
2022-01-19 14:58:23 +0100

    1

--- init.sh
+++ init.sh
@@ -145,7 +145,7 @@ echo
 
 banner2 "    R U N  P O R T A I N E R"
 #docker-compose -f /home/$USER/docker/docker-compose.yml up -d
-mv docker-compose.yml /home/$USER/docker
+mv docker-compose.yml /home/$USER/docker/docker-compose.yml
 docker-compose -f /home/$USER/docker/docker-compose.yml up -d
 docker ps
 
