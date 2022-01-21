56693e1cd5f2b53f8c93303e9e93df9a4b8dfb29
sekkigit <sekiuredjaji@gmail.com>
2022-01-21 21:15:37 +0100

    1

--- config/firewall.sh
+++ config/firewall.sh
@@ -8,10 +8,10 @@ ufw limit $PORTSSH/tcp
 ufw allow 80
 ufw allow 443
 ufw allow 8080
-ufw allow from 192.168.0.0/24 to any port 9090/tcp
-ufw allow from 192.168.0.0/24 to any port 9000/tcp
-ufw allow from 192.168.0.0/24 to any port 81
-ufw allow from 192.168.0.0/24 to any port Samba
+ufw allow from $SUBNET to any port 9090/tcp
+ufw allow from $SUBNET to any port 9000/tcp
+ufw allow from $SUBNET to any port 81
+ufw allow from $SUBNET to any port Samba
 ufw app update plexmediaserver
 ufw allow plexmediaserver-all
 ufw --force enable
