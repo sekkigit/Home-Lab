ce7cc3568af18dbdcf5607491947ef3b46a372ab
sekkigit <sekiuredjaji@gmail.com>
2022-01-24 08:52:13 +0100

    1

--- config/log.sh
+++ config/log.sh
@@ -4,22 +4,24 @@ source .var
 
 cat <<EOF > log.txt
 _________________________________________________________________________
-CREATED:
-  
+INFO:
+
+  OS VERSION:     $OSVER
+
   USER INFO:
      - Username:  $USER
      - Userpass:  $USERPASS
      - Subnet:    $SUBNET
      - Gateway:   $GATE4
   
-  OPEN IN WEB:
+  WEB:
 
      - Cockpit:   $IP:9090       
      - Portainer: $IP:9000
      - Plex:      $IP:32400/web
      - Public IP: $PUBIP
 
-  OPEN IN EXPLORER:
+  EXPLORER:
 
      - Samba:     $IP
     
