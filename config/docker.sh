80892f4a672a0d73c91966fc871c697f2bffe499
Senad Dizdarevic <sekiuredjaji@gmail.com>
2022-02-07 17:05:23 +0100

    1

--- config/docker.sh
+++ config/docker.sh
@@ -2,10 +2,9 @@
 
 source .var
 
-apt install docker.io -y
-apt install docker-compose -y
-
 cat <<EOF > /home/$USER/docker/.env
 USER="$USER"
 SITE="$SITE"
 EOF
+
+apt install docker.io -y && apt install docker-compose -y
