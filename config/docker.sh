b11cd13a5a78e1dac8b381207813bf044f4219f5
sekkigit <sekiuredjaji@gmail.com>
2022-02-11 22:46:42 +0100

    1

--- config/docker.sh
+++ config/docker.sh
@@ -3,6 +3,7 @@
 source .var
 
 apt install docker.io -y && apt install docker-compose -y
+docker network create proxy
 cp ./config/docker-compose.yml /home/$USER/docker/docker-compose.yml
 
 cat <<EOF > /home/$USER/docker/.env
