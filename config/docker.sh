1780181c2f5e231199f4f23396e11ffc26701994
sekkigit <sekiuredjaji@gmail.com>
2022-02-11 22:47:58 +0100

    1

--- config/docker.sh
+++ config/docker.sh
@@ -11,18 +11,3 @@ USER=$USER
 DNSAPI=$DNSAPI
 SITE=$SITE
 EOF
-
-cat <<EOF > /home/$USER/docker/nginx/config.json
-{
-  "database": {
-    "engine": "knex-native",
-    "knex": {
-      "client": "sqlite3",
-      "connection": {
-        "filename": "/data/database.sqlite"
-      },
-      "useNullAsDefault": true
-    }
-  }
-}
-EOF
