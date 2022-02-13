1c8bfc168cf053ba834ede18874cc2bf61b7d067
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-02-13 15:51:20 +0100

    1

--- config/firewall.sh
+++ config/firewall.sh
@@ -6,15 +6,15 @@ ufw app update plexmediaserver
 ufw default reject incoming
 ufw default allow outgoing
 ufw limit $PORTSSH/tcp
-ufw allow 80
-ufw allow 443
-ufw allow 9010
-ufw allow from $SUBNET to any port 9055
-ufw allow from $SUBNET to any port 9000
-ufw allow from $SUBNET to any port 4020
-ufw allow from $SUBNET to any port 3005
-ufw allow from $SUBNET to any port 3006
-ufw allow from $SUBNET to any port 81
+ufw allow 80   #HTTP
+ufw allow 443  #HTTPS
+ufw allow 9090 #Cockpit
+ufw allow 9055 #Homr
+ufw allow 9010 #Prometheus
+ufw allow 9000 #Portainer
+ufw allow 4020 #Grafana
+ufw allow 3005 #SpeedTest
+ufw allow 81   #Nginx
 ufw allow from $SUBNET to any app Samba
 ufw allow plexmediaserver-all
 ufw --force enable
