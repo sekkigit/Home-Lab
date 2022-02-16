26bb756159eeedcc78a1b21519f4156e551d5c0e
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-02-16 19:30:29 +0100

    1

--- config/autotask.sh
+++ config/autotask.sh
@@ -4,4 +4,5 @@ source .var
 
 cat <<EOF >> /etc/crontab
 $TASKRUN root    apt update && apt upgrade -y
+$TASKRUN root    cscli hub update && cscli collections upgrade crowdsecurity/sshd && systemctl reload crowdsec
 EOF
