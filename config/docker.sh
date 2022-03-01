9247dca2c19eb8c50fe3172fcbb2b06ca325b52c
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-03-01 22:55:19 +0100

    1

--- config/docker.sh
+++ config/docker.sh
@@ -8,6 +8,8 @@ cp ./config/docker-compose.yml /home/$USER/docker/docker-compose.yml
 
 cat <<EOF > /home/$USER/docker/.env
 USER=${USER}
+TIMEZONE=${TIMEZONE}
+IP=${IP}
 DNSAPI=${DNSAPI}
 SITE=${SITE}
 ROOT_PASSWORD=${ROOT_PASSWORD}
