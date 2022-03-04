86e08b97f2fe23870d46528f17fc018845335af2
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-03-04 18:58:37 +0100

    1

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -2,13 +2,13 @@
 
 source .var
 
-apt install ca-certificates wget net-tools gnupg -y
+apt update && apt -y install ca-certificates wget net-tools gnupg
 wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
 echo "deb http://as-repository.openvpn.net/as/debian focal main">/etc/apt/sources.list.d/openvpn-as-repo.list
-apt update
-apt install openvpn-as -y
+apt update && apt -y install openvpn-as
+
 
 sudo sysctl net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -s 10.8.0.0/16 -o ens5 -j MASQUERADE
-iptables -t nat -I POSTROUTING -s 10.8.0.0/16 -d 192.168.1.0/16 -o ens5 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -I POSTROUTING -s 192.168.1.0/24 -d 192.168.1.0/24 -o eth0 -j MASQUERADE
 iptables-save
