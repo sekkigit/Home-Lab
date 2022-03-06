b8153ee611f94cc581a575a90cc277690ab0fbb9
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-03-06 17:33:12 +0100

    1

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source .var
+
 apt update && apt -y install ca-certificates wget net-tools gnupg
 wget -qO - https://as-repository.openvpn.net/as-repo-public.gpg | apt-key add -
 echo "deb http://as-repository.openvpn.net/as/debian focal main">/etc/apt/sources.list.d/openvpn-as-repo.list
@@ -8,10 +10,11 @@ apt update && apt -y install openvpn-as
 cat <<EOF >> /etc/sysctl.conf
 net.ipv4.ip_forwarding=1
 EOF
+
 sysctl net.ipv4.ip_forwarding=1
 sysctl net.ipv4.ip_forwarding
-iptables -t nat -A POSTROUTING -s 10.8.0.0/16 -o ens5 -j MASQUERADE
-iptables -t nat -I POSTROUTING -s 10.8.0.0/16 -d 192.168.0.0/16 -o ens5 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s $GATE4 -o $NETADAPT -j MASQUERADE
+iptables -t nat -I POSTROUTING -s $GATE4 -d $SUBNET -o $NETADAPT -j MASQUERADE
 iptables-save
 #ip route
 #sudo ip route del default via 192.168.0.1
